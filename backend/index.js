73d6f6ad4245bf70b9cb088f6b2ef806c1a841ce
Ebe <beatsbyebe@gmail.com>
2017-05-15 14:04:54 +0200

    add encode

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,93 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const app = express()
+var Kodolo = require('caesar-ciphers').defaultCipher;
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/api', (req, res) => {
-    res.status(200).json({
-        hello: "world"
-    })
+var currEltolas = 3
+var kulcsok = [{
+    kulcs: 'asd', eltolas: 1
+}, {
+    kulcs: 'asdER', eltolas: 2
+}]
+
+function ismertEAKulcs(input) {
+    for (let kulcs of kulcsok) {
+        if (kulcs.kulcs == input) {
+            return kulcs.eltolas
+        }
+    }
+    return -1
+}
+
+function kovetkezoEltolas() {
+    currEltolas++
+    if (currEltolas > 20) {
+        currEltolas = 3
+    }
+    return currEltolas
+}
+
+function onlyEnglishChars(input) {
+    // var regex = new RegExp('/^[a-zA-Z]+$/')
+    return input.match(/^[a-zA-Z]+$/gi)
+}
+
+app.post('/api', (req, res) => {
+    var tipus = req.body.tipus
+    var kulcs = req.body.kulcs
+    var szoveg = req.body.szoveg
+
+    var helyesE = tipus && kulcs && szoveg &&
+        (tipus == 'encode' || tipus == 'decode') &&
+        tipus.trim().length > 0 && kulcs.trim().length > 0 && szoveg.trim().length > 0 &&
+        onlyEnglishChars(szoveg) != null
+
+    if (helyesE) {
+        var eltolas = ismertEAKulcs(kulcs)
+        if (tipus == 'encode') {
+            if (eltolas != -1) {
+                //res.send("ISMERT!! (encode)")
+                var kodolo = new Kodolo(eltolas)
+                var kodoltSzoveg = kodolo.encrypt(szoveg)
+                res.status(200).json({
+                    kodoltSzoveg: kodoltSzoveg
+                })
+                //return
+            } else {
+                //res.send("NEM ISMERT")
+                eltolas = kovetkezoEltolas()
+                kulcsok.push({
+                    kulcs: kulcs,
+                    eltolas: eltolas
+                })
+                var kodolo = new Kodolo(eltolas)
+                var kodoltSzoveg = kodolo.encrypt(szoveg)
+                res.status(200).json({
+                    kodoltSzoveg: kodoltSzoveg
+                })
+                //return
+            }
+        } else if (tipus == 'decode') {
+            if (eltolas != -1) {
+                res.send("ISMERT!! (decode)")
+            } else {
+                //res.send("NEM ISMERT")
+                //return
+            }
+        }
+    } else {
+        res.status(400).json({
+            status: 'Bad Request'
+        })
+        //return
+    }
 })
 
 app.get('*', (req, res) => {
     res.status(404).json({
-        status: 'not found'
+        status: 'Not Found'
     })
 })
 
