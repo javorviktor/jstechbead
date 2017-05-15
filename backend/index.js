f655bcb094e1baf5be4fe94c496e2850662ce35a
Ebe <beatsbyebe@gmail.com>
2017-05-15 14:08:12 +0200

    add decode

--- backend/index.js
+++ backend/index.js
@@ -48,15 +48,12 @@ app.post('/api', (req, res) => {
         var eltolas = ismertEAKulcs(kulcs)
         if (tipus == 'encode') {
             if (eltolas != -1) {
-                //res.send("ISMERT!! (encode)")
                 var kodolo = new Kodolo(eltolas)
                 var kodoltSzoveg = kodolo.encrypt(szoveg)
                 res.status(200).json({
-                    kodoltSzoveg: kodoltSzoveg
+                    kodoltSzoveg
                 })
-                //return
             } else {
-                //res.send("NEM ISMERT")
                 eltolas = kovetkezoEltolas()
                 kulcsok.push({
                     kulcs: kulcs,
@@ -65,23 +62,27 @@ app.post('/api', (req, res) => {
                 var kodolo = new Kodolo(eltolas)
                 var kodoltSzoveg = kodolo.encrypt(szoveg)
                 res.status(200).json({
-                    kodoltSzoveg: kodoltSzoveg
+                    kodoltSzoveg
                 })
-                //return
             }
         } else if (tipus == 'decode') {
             if (eltolas != -1) {
-                res.send("ISMERT!! (decode)")
+                //res.send("ISMERT!! (decode)")
+                var kodolo = new Kodolo(eltolas)
+                var dekodoltSzoveg = kodolo.decrypt(szoveg)
+                res.status(200).json({
+                    dekodoltSzoveg
+                })
             } else {
-                //res.send("NEM ISMERT")
-                //return
+                res.status(403).json({
+                    status: 'Unknown password'
+                })
             }
         }
     } else {
         res.status(400).json({
             status: 'Bad Request'
         })
-        //return
     }
 })
 
